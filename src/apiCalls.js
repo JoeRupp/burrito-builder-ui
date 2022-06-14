82988a292f578bb3273de9278c7cd7a0c5ab4ccc
Joe Rupp <joerpp8@gmail.com>
2022-06-14 10:00:20 -0600

    Add make order fetch request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,6 +10,22 @@ const apiCalls = {
       })
       .catch((err) => console.log(err));
   },
+
+  makeOrder: (order) => {
+    return fetch("http://localhost:3001/api/v1/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(order),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response);
+        } else {
+          return response.json();
+        }
+      })
+      .catch((err) => console.log(err));
+  },
 };
 
 export default apiCalls;
